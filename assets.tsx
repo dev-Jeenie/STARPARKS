382037b5cbfa9ccdee41126a83b492adfb1f546b
jeenie <jeenie@actbase.io>
2022-02-08 05:29:46 +0900

    add assets

---
+++ assets.tsx
@@ -0,0 +1,9 @@
+export default {
+  icon_home: require('./src/assets/icon_home.png'),
+  icon_card: require('./src/assets/icon_card.png'),
+  icon_coffee: require('./src/assets/icon_coffee.png'),
+  icon_dots: require('./src/assets/icon_dots.png'),
+  icon_drink: require('./src/assets/icon_drink.png'),
+  icon_gift: require('./src/assets/icon_gift.png'),
+  logo_starbucks: require('./src/assets/logo_starbucks.png'),
+};
